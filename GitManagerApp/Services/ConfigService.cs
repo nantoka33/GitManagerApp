573951c49d40f62549fc4a81bc1117295151d5ec
grosswolf <44463045+grosswolf@users.noreply.github.com>
2025-06-26 11:18:28 +0900

    コンフィグコメントの追加

--- GitManagerApp/Services/ConfigService.cs
+++ GitManagerApp/Services/ConfigService.cs
@@ -5,21 +5,39 @@ using System.Text.Json;
 
 namespace GitManagerApp.Services
 {
+    /// <summary>
+    /// コンフィグサービスクラス
+    /// </summary>
     public class ConfigService
     {
+        /// <summary>
+        /// コンフィグファイルのパス
+        /// </summary>
         private readonly string configPath;
 
+        /// <summary>
+        /// コンフィグサービスのコンストラクタ
+        /// </summary>
+        /// <param name="configFilePath">コンフィグファイルのパス</param>
         public ConfigService(string configFilePath)
         {
             configPath = configFilePath;
         }
 
+        /// <summary>
+        /// コンフィグを保存するメソッド
+        /// </summary>
+        /// <param name="config">コンフィグ</param>
         public void Save(AppConfig config)
         {
             var json = JsonSerializer.Serialize(config);
             File.WriteAllText(configPath, json);
         }
 
+        /// <summary>
+        /// コンフィグをロードするメソッド
+        /// </summary>
+        /// <returns>コンフィグ</returns>
         public AppConfig? Load()
         {
             if (!File.Exists(configPath)) return null;
